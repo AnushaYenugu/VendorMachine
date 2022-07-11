ce8fe477f14cd4deb9ec1ce482cfa737c2c6f16d
Anusha Yenugu <anushayenugu92@gmail.com>
2022-07-11 15:48:06 +0200

    1st version
    added changes

--- src/test/java/lexicon/fundamentals/oop/model/VendingMachineImplTest.java
+++ src/test/java/lexicon/fundamentals/oop/model/VendingMachineImplTest.java
@@ -1,9 +1,10 @@
 package lexicon.fundamentals.oop.model;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static Assertions.*;
+//import static Assertions.*;
 
 class VendingMachineImplTest {
     VendingMachineImpl sut;
@@ -17,30 +18,53 @@ class VendingMachineImplTest {
         Fruits orange = new Fruits(2,20, "orange", "fruity");
         testProducts[1] = orange;
         sut = new VendingMachineImpl(amt, testProducts);
+        sut.addCurrency(200);
     }
 
     @Test
     void addCurrency() {
         sut.addCurrency(200);
+      //  Assertions.assertEquals(200,sut.addCurrency(200));
     }
 
     @Test
     void getBalance() {
+
+        Assertions.assertEquals(200,sut.getBalance());
+
     }
 
     @Test
     void request() {
+        sut.request(1);
     }
 
     @Test
     void endSession() {
+        Assertions.assertEquals(0,sut.endSession());
     }
 
     @Test
     void getDescription() {
+        Assertions.assertEquals("Drink from here",sut.getDescription(1));
+        Assertions.assertEquals("Fruits are here",sut.getDescription(2));
     }
 
     @Test
     void getProducts() {
+        Assertions.assertAll(
+
+                ( )->  Assertions.assertEquals(1,testProducts[0].getId()),
+                ()->   Assertions.assertEquals("CocoCola",testProducts[0].getProductName()),
+                ()->   Assertions.assertEquals(10,testProducts[0].getPrice())
+          );
+
+
+
+    }
+
+    @Test
+    void change(){
+        Assertions.assertEquals(170,sut.change());
     }
 }
